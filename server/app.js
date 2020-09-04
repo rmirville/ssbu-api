6463e44b4afc6bc8d823b8cbd1fc1a5922e77c57
Raynald Mirville <raynaldm@pm.me>
2020-09-04 12:33:35 -0400

    Reorganized file structure

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var stagesRouter = require('./routes/stages');
+var stagesRouter = require('./features/stages/stages-router');
 
 var app = express();
 
