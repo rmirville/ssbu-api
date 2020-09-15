c2a29c57df968d1fad6ab35b2777a2726fabfcc0
Raynald Mirville <raynaldm@pm.me>
2020-09-15 14:44:10 -0400

    Config: Added coverage for undefined ports

--- server/config/server.js
+++ server/config/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const hostname = process.env.SSBUTOOLS_API_HOST;
 
 let portTemp = process.env.SSBUTOOLS_API_PORT;
-const port = ((portTemp !== undefined) && (portTemp.length > 0)) ? portTemp : null;
+const port = ((typeof portTemp !== 'undefined') && (portTemp.length > 0)) ? portTemp : null;
 
 const serverConfig = {hostname, port};
 Object.freeze(serverConfig);
