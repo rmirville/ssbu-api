fbd8d50f10df9d137acd4210d86376347162d000
Raynald Mirville <raynaldm@pm.me>
2020-09-23 16:44:07 -0400

    DBService::connect() - added data validation

--- server/core/persistence/db-service.js
+++ server/core/persistence/db-service.js
@@ -4,21 +4,43 @@ class DBService {
   constructor() { }
 
   static connect(type, hostname, port, dbname, username, secret) {
-    // assuming type = 'mongodb'
-    return this._connectMongo(hostname, port, dbname, username, secret);
+
+    try {
+      this._validateParams(type, hostname, port, dbname, username, secret)
+    } catch (err) {
+      return new Promise(
+        (resolve, reject) => {
+          reject(err);
+        }
+      )
+    }
+
+    return this._connectMongo(hostname, dbname, username, secret);
   }
 
-  static _connectMongo(hostname, port, dbname, username, secret) {
-
+  static _connectMongo(hostname, dbname, username, secret) {
     const urlPrefix = `mongodb+srv://${username}:${secret}@${hostname}`;
-
-    const urlPort = (port !== null) ? `:${port}` : '';
 
     const urlSuffix = `/${dbname}?retryWrites=true&w=majority`;
 
-    const url = urlPrefix + urlPort + urlSuffix;
-
+    const url = urlPrefix + urlSuffix;
     return mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+  }
+
+  static _validateParams(type, hostname, port, dbname, username, secret) {
+    const stringParams = {type, hostname, dbname, username, secret};
+
+    for (const param in stringParams) {
+      if (stringParams.hasOwnProperty(param)) {
+        if (typeof stringParams[param] !== 'string') {
+          throw new TypeError(`${param} is not of type string`);
+        }
+      }
+    }
+
+    if ((typeof port !== 'number') && (port !== null)) {
+      throw new TypeError('port is neither null or of type number');
+    }
   }
 }
 
--- server/core/persistence/db-service.spec.js
+++ server/core/persistence/db-service.spec.js
@@ -2,82 +2,164 @@ require('dotenv').config();
 
 const { DBService } = require('./db-service');
 
-describe('DBService', () => {
+describe('DBService -', () => {
   const mongoose = require('mongoose');
 
   const type = 'mongodb';
   const host = process.env.SSBUTOOLS_DB_MONGO_R_HOST;
   let portTemp = process.env.SSBUTOOLS_DB_MONGO_R_PORT;
-  const port = ((typeof portTemp !== 'undefined') && (portTemp.length > 0)) ? portTemp : null;
+  const port = ((typeof portTemp !== 'undefined') && (portTemp.length > 0)) ? parseInt(portTemp) : null;
   const dbname = process.env.SSBUTOOLS_DB_MONGO_R_DB;
   const user = process.env.SSBUTOOLS_DB_MONGO_R_USER;
   const pass = process.env.SSBUTOOLS_DB_MONGO_R_SECRET;
 
   const mongoArgs = [type, host, port, dbname, user, pass];
 
-  describe('DBService', () => {
+  describe('DBService -', () => {
 
-    describe('connect()', () => {
+    describe('connect() -', () => {
 
-      describe('type mongodb', () => {
+      describe('type mongodb -', () => {
 
-        describe('output', () => {
-          
-          it('is an instance of Promise', () => {
+        afterEach(async () => {
+          /// console.log('=== SPEC - AFTER - closing mongoose connection');
+          await mongoose.connection.close();
+          await mongoose.disconnect();
+          /// console.log('=== SPEC - AFTER - mongoose connection closed');
+        });
+
+        describe('output -', () => {
+
+          it('is an instance of Promise', async () => {
+            /// console.group('\n=== SPEC - connect output - instance of Promise');
             const output = DBService.connect(...mongoArgs);
+            /// console.log('SPEC - connect finished');
+            /// console.log(`instance of Promise: ${output instanceof Promise}`);
             expect(output).toBeInstanceOf(Promise);
+            await output;
           });
 
           it('resolves to a Mongoose instance', async () => {
             /// console.group('\n=== SPEC - resolves to a Mongoose instance');
-            await DBService.connect(...mongoArgs).then(
-              (value) => {
-
-                /// console.log('SPEC - promise fulfilled');
-
-                expect(value).toBeInstanceOf(mongoose.Mongoose, 'should be a Mongoose instance');
-
-              },
-              (err) => {
-
-                fail('method should connect successfully');
-
-              }
-            );
-            /// console.groupEnd();
+            const output = await DBService.connect(...mongoArgs);
+            expect(output).toBeInstanceOf(mongoose.Mongoose);
           });
           
           it('creates a valid MongoDB connection', async () => {
-            /// console.group('\n=== SPEC - creates a valid mongodb connection:');
-            await DBService.connect(...mongoArgs).then(
-              async function(value) {
-
-                /// console.log('SPEC - promise fulfilled');
-
-                expect(value.connection).toBeInstanceOf(mongoose.Connection, 'should have a connection property');
-
-                await value.connection.startSession();
-
-                /// console.log(`SPEC - readyState: ${JSON.stringify(value.connection.readyState)}`);
-
-                expect(value.connection.readyState).withContext('should be connected').toBe(1);
-
-              },
-              (err) => {
-
-                /// console.log(JSON.stringify(err));
-                fail('method should connect successfully');
-
-              }
-            );
-            /// console.groupEnd();
+            /// console.group('\n=== SPEC - connect output - creates a valid mongodb connection:');
+            const output = await DBService.connect(...mongoArgs);
+            expect(output.connection).toBeInstanceOf(mongoose.Connection, 'should have a connection property');
+            await output.connection.startSession();
+            expect(output.connection.readyState).withContext('should be connected').toBe(1);
           });
         
         });
 
       });
 
-      describe('data validation', () => {
+      describe('data validation -', () => {
+
+        it('should reject a non-string type', async () => {
+          /// console.group('\n=== SPEC - connect validation - reject type type');
+          let badArgs = [47, host, port, dbname, user, pass];
+
+          await DBService.connect(...badArgs).then(
+            value => {
+              fail('connect() should throw an exception');
+            },
+            err => {
+              expect(err).toBeInstanceOf(TypeError);
+            }
+          ).then(
+            () => {
+              /// console.groupEnd();
+          });
+        });
+
+        it('should reject a non-string host', async () => {
+          /// console.group('\n=== SPEC - connect validation - reject host type');
+          let badArgs = [type, false, port, dbname, user, pass];
+
+          await DBService.connect(...badArgs).then(
+            value => {
+              fail('connect() should throw an exception');
+            },
+            err => {
+              expect(err).toBeInstanceOf(TypeError);
+            }
+          ).then(
+            () => {
+              /// console.groupEnd();
+          });
+        });
+
+        it('should reject a non-number/null port', async () => {
+          /// console.group('\n=== SPEC - connect validation - reject port type');
+          let badArgs = [type, host, '3000', dbname, user, pass];
+
+          await DBService.connect(...badArgs).then(
+            value => {
+              fail('connect() should throw an exception');
+            },
+            err => {
+              expect(err).toBeInstanceOf(TypeError);
+            }
+          ).then(
+            () => {
+              /// console.groupEnd();
+          });
+        });
+
+        it('should reject a non-string dbname', async () => {
+          /// console.group('\n=== SPEC - connect validation - reject dbname type');
+          let badArgs = [type, host, port, 38.40, user, pass];
+
+          await DBService.connect(...badArgs).then(
+            value => {
+              fail('connect() should throw an exception');
+            },
+            err => {
+              expect(err).toBeInstanceOf(TypeError);
+            }
+          ).then(
+            () => {
+              /// console.groupEnd();
+          });
+        });
+
+        it('should reject a non-string username', async () => {
+          /// console.group('\n=== SPEC - connect validation - reject username type');
+          let badArgs = [type, host, port, dbname, {prop: 'name'}, pass];
+
+          await DBService.connect(...badArgs).then(
+            value => {
+              fail('connect() should throw an exception');
+            },
+            err => {
+              expect(err).toBeInstanceOf(TypeError);
+            }
+          ).then(
+            () => {
+              /// console.groupEnd();
+          });
+        });
+
+        it('should reject a non-string secret', async () => {
+          /// console.group('\n=== SPEC - connect validation - reject secret type');
+          let badArgs = [type, host, port, dbname, user, -93.1];
+
+          await DBService.connect(...badArgs).then(
+            value => {
+              fail('connect() should throw an exception');
+            },
+            err => {
+              expect(err).toBeInstanceOf(TypeError);
+            }
+          ).then(
+            () => {
+              /// console.groupEnd();
+          });
+        });
 
       });
 
