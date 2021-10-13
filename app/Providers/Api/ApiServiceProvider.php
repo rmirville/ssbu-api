f0020bdee48b878edcec73d1e5bab67d291f2dd4
Raynald Mirville <raynaldm@pm.me>
2021-10-13 16:05:06 -0400

    feat(api): remove 'data' resource wrapping

---
+++ app/Providers/Api/ApiServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers\Api;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\ServiceProvider;
+
+class ApiServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        JsonResource::withoutWrapping();
+    }
+}
